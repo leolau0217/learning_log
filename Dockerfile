04ca81ccd4abd5b50772164f6918f95a24b1bfac
leolau0217 <leolau0217@gmail.com>
2023-03-10 15:01:42 +0800

    First edition

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.10.6
+
+LABEL maintainer="LeoLau0217@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
---
+++ users/urls.py
@@ -0,0 +1,21 @@
+"""
+定义users的URL模式
+"""
+
+from django.urls import path
+from django.contrib.auth.views import LoginView,LogoutView
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # 登录页面
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # 注销页面
+    path('logout/', views.logout_view, name='logout'),
+
+    # 注册页面
+    path('register/', views.register, name='register'),
+]
